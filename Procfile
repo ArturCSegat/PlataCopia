dd992d4cf2c455012d02bb895f3f1be21aef0272
EpicLordFnaf <80114342+EpicLordFnaf@users.noreply.github.com>
2021-09-14 17:49:01 -0300

    fuck heroku 2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-gunicorn "website:create_app()"
+gunicorn -w 4 -b 0.0.0.0:5000 run:create_app('dev')
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from website import create_app
+import os
 
 
 if __name__ == "__main__":
-    app = create_app()
+    app = create_app(os.getenv('FLASK_CONFIG') or 'dev')
     app.run(debug=True)
